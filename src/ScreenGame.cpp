404206a37a07bf9949254b5e68a5ae64883cdc76
Hopson97 <mhopson@hotmail.co.uk>
2021-08-07 09:19:20 +0100

    Switch tile map to use vertex arrat]

--- src/ScreenGame.cpp
+++ src/ScreenGame.cpp
@@ -21,6 +21,7 @@ ScreenGame::ScreenGame(ScreenManager* stack)
     m_view.setSize({1600, 900});
     m_view.zoom(m_currentZoom);
 
+
     for (int i = 0; i < WORLD_SIZE + 1; i++) {
         // west-north grid lines
         auto startPos = tileToScreenPosition(0, i);
@@ -63,12 +64,20 @@ ScreenGame::ScreenGame(ScreenManager* stack)
     // "Auto tile" the tilemap
     for (int y = WORLD_SIZE - 1; y >= 0; y--) {
         for (int x = 0; x < WORLD_SIZE; x++) {
+            auto pos = tileToScreenPosition(x, y);
+
+            m_tileVerts.emplace_back(pos, sf::Vector2f{0, 0});
+            m_tileVerts.emplace_back(sf::Vector2f{pos.x, pos.y + TILE_HEIGHT}, sf::Vector2f{0, TILE_HEIGHT});
+            m_tileVerts.emplace_back(sf::Vector2f{pos.x + TILE_WIDTH, pos.y + TILE_HEIGHT}, sf::Vector2f{TILE_WIDTH, TILE_HEIGHT});
+            m_tileVerts.emplace_back(sf::Vector2f{pos.x + TILE_WIDTH, pos.y}, sf::Vector2f{TILE_WIDTH, 0});
+
+
             Tile* tile = getTile(sf::Vector2i{x, y});
             if (tile->type != TileType::Grass)
                 for (int i = 0; i < 4; i++) {
                     Tile* neighbour = getTile(sf::Vector2i{x, y} + TILE_OFFSETS[i]);
                     if (neighbour && neighbour->type == tile->type) {
-                        tile->varient += std::pow(2, i);
+                     //   tile->varient += std::pow(2, i);
                     }
                 }
         }
@@ -153,14 +162,23 @@ void ScreenGame::onEvent(const sf::Event& e)
         m_mousedown = false;
     }
     else if (e.type == sf::Event::MouseWheelScrolled) {
-        if (e.mouseWheelScroll.delta > 0 && m_currentZoom > 0.065) {
+        if (e.mouseWheelScroll.delta > 0 && m_currentZoom == 8) {
+            m_view.zoom(1.0f/8.0f);
+            m_currentZoom *= 1.0f/8.0f;
+        }
+        else if (e.mouseWheelScroll.delta > 0 && m_currentZoom > 0.065) {
             m_view.zoom(0.5f);
             m_currentZoom *= 0.5f;
         }
-        else if (e.mouseWheelScroll.delta < 0 && m_currentZoom < 1.99) {
+        else if (e.mouseWheelScroll.delta < 0 && m_currentZoom < 1) {
             m_view.zoom(2.0f);
             m_currentZoom *= 2.0f;
         }
+        else if (e.mouseWheelScroll.delta < 0 && (int)m_currentZoom == 1) {
+            m_view.zoom(8.0f);
+            m_currentZoom = 8.0f;
+        }
+
         std::cout << m_currentZoom << std::endl;
     }
 
@@ -233,62 +251,69 @@ void ScreenGame::onRender(sf::RenderWindow* window)
 
     int count = 0;
 
-    for (int y = WORLD_SIZE - 1; y >= 0; y--) {
-        for (int x = 0; x < WORLD_SIZE; x++) {
-            sf::Vector2f screenPosition = tileToScreenPosition(x, y);
+    //for (int y = WORLD_SIZE - 1; y >= 0; y--) {
+    //    for (int x = 0; x < WORLD_SIZE; x++) {
+    //        sf::Vector2f screenPosition = tileToScreenPosition(x, y);
+//
+    //        // Cull tiles not in the current viewport
+    //        sf::Vector2f windowPosition = {
+    //            screenPosition.x - m_originOffset.x * TILE_WIDTH,
+    //            screenPosition.x - m_originOffset.y * TILE_HEIGHT};
+    //        if (screenPosition.x <
+    //                m_view.getCenter().x - m_view.getSize().x / 2 - TILE_WIDTH ||
+    //            screenPosition.x > m_view.getCenter().x + m_view.getSize().x / 2 ||
+    //            screenPosition.y <
+    //                m_view.getCenter().y - m_view.getSize().y / 2 - TILE_HEIGHT ||
+    //            screenPosition.y > m_view.getCenter().y + m_view.getSize().y / 2) {
+    //            continue;
+    //        }
+    //        count++;
+    //        m_tileRect.setPosition(screenPosition);
+//
+    //        // Set the tile texture
+    //        const Tile* tile = getTile({x, y});
+    //        if (tile->type == TileType::Grass) {
+    //            m_tileRect.setTextureRect(sf::IntRect{tile->varient * (int)TILE_WIDTH, 0,
+    //                                                  (int)TILE_WIDTH, (int)TILE_HEIGHT});
+    //        }
+    //        else if (tile->type == TileType::Road) {
+    //            m_tileRect.setTextureRect(sf::IntRect{tile->varient * (int)TILE_WIDTH,
+    //                                                  (int)TILE_HEIGHT, (int)TILE_WIDTH,
+    //                                                  (int)TILE_HEIGHT});
+    //        }
+    //        else if (tile->type == TileType::Water) {
+    //            m_tileRect.setTextureRect(sf::IntRect{m_wateranim.getFrame().left,
+    //                                                  (int)TILE_HEIGHT * 3,
+    //                                                  (int)TILE_WIDTH, (int)TILE_HEIGHT});
+    //            window->draw(m_tileRect);
+    //            m_tileRect.setTextureRect(sf::IntRect{tile->varient * (int)TILE_WIDTH,
+    //                                                  (int)TILE_HEIGHT * 2,
+    //                                                  (int)TILE_WIDTH, (int)TILE_HEIGHT});
+    //        }
+//
+    //        // Render the tile
+    //        window->draw(m_tileRect);
+    //    }
+    //}
+    sf::RenderStates state = sf::RenderStates::Default;
+    state.texture = &m_tilemap;
+    window->draw(m_tileVerts.data(), m_tileVerts.size(), sf::Quads, state);
 
-            // Cull tiles not in the current viewport
-            sf::Vector2f windowPosition = {
-                screenPosition.x - m_originOffset.x * TILE_WIDTH,
-                screenPosition.x - m_originOffset.y * TILE_HEIGHT};
-            if (screenPosition.x <
-                    m_view.getCenter().x - m_view.getSize().x / 2 - TILE_WIDTH ||
-                screenPosition.x > m_view.getCenter().x + m_view.getSize().x / 2 ||
-                screenPosition.y <
-                    m_view.getCenter().y - m_view.getSize().y / 2 - TILE_HEIGHT ||
-                screenPosition.y > m_view.getCenter().y + m_view.getSize().y / 2) {
-                continue;
-            }
-            count++;
-            m_tileRect.setPosition(screenPosition);
-
-            // Set the tile texture
-            const Tile* tile = getTile({x, y});
-            if (tile->type == TileType::Grass) {
-                m_tileRect.setTextureRect(sf::IntRect{tile->varient * (int)TILE_WIDTH, 0,
-                                                      (int)TILE_WIDTH, (int)TILE_HEIGHT});
-            }
-            else if (tile->type == TileType::Road) {
-                m_tileRect.setTextureRect(sf::IntRect{tile->varient * (int)TILE_WIDTH,
-                                                      (int)TILE_HEIGHT, (int)TILE_WIDTH,
-                                                      (int)TILE_HEIGHT});
-            }
-            else if (tile->type == TileType::Water) {
-                m_tileRect.setTextureRect(sf::IntRect{m_wateranim.getFrame().left,
-                                                      (int)TILE_HEIGHT * 3,
-                                                      (int)TILE_WIDTH, (int)TILE_HEIGHT});
-                window->draw(m_tileRect);
-                m_tileRect.setTextureRect(sf::IntRect{tile->varient * (int)TILE_WIDTH,
-                                                      (int)TILE_HEIGHT * 2,
-                                                      (int)TILE_WIDTH, (int)TILE_HEIGHT});
-            }
-
-            // Render the tile
-            window->draw(m_tileRect);
-        }
-    }
     m_tileRect.setTextureRect(sf::IntRect{0, 0, (int)TILE_WIDTH, (int)TILE_HEIGHT});
     m_tileRect.setTexture(&m_selectionTexture);
     m_tileRect.setPosition(tileToScreenPosition(m_selectedTile.x, m_selectedTile.y));
     window->draw(m_tileRect);
 
-    if (drawGrid) {
+    if (drawGrid && (int)m_currentZoom != 8) {
         window->draw(m_grid.data(), m_grid.size(), sf::Lines);
     }
+
     if (ImGui::Begin("Info")) {
         ImGui::Text("Tile: %d %d", m_selectedTile.x, m_selectedTile.y);
         ImGui::Text("Drawn: %d / %d", count, (int)m_tiles.size());
         ImGui::Checkbox("Draw Grid", &drawGrid);
     }
     ImGui::End();
+    ImGui::ShowMetricsWindow(nullptr);
+
 }
