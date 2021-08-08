a3bf99c681d65905f9e57e3c9753db2c5f3c663c
Hopson97 <mhopson@hotmail.co.uk>
2021-08-08 20:06:27 +0100

    Split out profier code to .cpp file, use float to store time value

---
+++ src/Profiler.cpp
@@ -0,0 +1,64 @@
+#include "Profiler.h"
+
+Profiler::Profiler() { m_last50times.resize(50); };
+
+void Profiler::reset()
+{
+    auto time = m_internalClock.restart();
+
+    frametime = time.asSeconds();
+    m_last50times.push_back(frametime);
+    if (m_last50times.size() > 50) {
+        m_last50times.pop_front();
+    }
+
+    float tmpavg = 0;
+    for (auto& x : m_last50times) {
+        tmpavg += x;
+    }
+    frametimeAverage = tmpavg / 0.05f;
+
+    for (auto& x : m_activeslots) {
+
+        if (!x.stopped) {
+            x.stop();
+            m_lastSlots.emplace_back(std::make_pair("ForgotToStop: " + x.name, 0.f));
+            continue;
+        }
+        m_lastSlots.emplace_back(
+            std::make_pair(x.name, Mode == ProfilerMode::Milliseconds
+                                       ? x.getTime().asSeconds() * 1000
+                                       : x.getTime().asSeconds() * 1000 * 1000));
+    }
+    m_activeslots.clear();
+}
+
+void Profiler::onGUI()
+{
+    if (ImGui::Begin("Profiler")) {
+        ImGui::TextColored({0, 0.392f, 0.960f, 1}, "Current Frametime: %f ms",
+                           static_cast<float>(frametime * 1000.0f));
+
+        std::array<float, 50> temptimes;
+        for (unsigned x = 0; x < temptimes.size(); x++) {
+            temptimes[x] = m_last50times[x];
+        }
+        ImGui::PlotHistogram("##Times", temptimes.data(), m_last50times.size());
+        ImGui::Text("Avg: %.2f ms,(%.1f fps)", frametimeAverage,
+                    1000.f / frametimeAverage);
+        ImGui::Separator();
+        ImGui::TextColored({0, 0.6f, 0.188f, 1}, "Times in %s",
+                           Mode == ProfilerMode::Milliseconds ? "Milliseconds"
+                                                              : "Microseconds");
+        for (auto& ts : m_lastSlots) {
+            ImGui::Text("%s: %f", ts.first.c_str(), ts.second);
+        }
+    }
+    ImGui::End();
+    m_lastSlots.clear();
+}
+
+TimeSlot& Profiler::newTimeslot(const char* name)
+{
+    return m_activeslots.emplace_back(name);
+};
--- src/Profiler.h
+++ src/Profiler.h
@@ -2,96 +2,55 @@
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <imgui_sfml/imgui-SFML.h>
 #include <imgui_sfml/imgui.h>
-//cross platform memory usage thingy
+#include <deque>
+#include <array>
 
-class TimeSlot{
+
+class TimeSlot {
   public:
-    TimeSlot(std::string _name): name(_name){
+    TimeSlot(std::string _name)
+        : name(_name)
+    {
         begin = _internalClock.restart();
         end = begin;
-
     }
 
-    void stop(){
+    void stop()
+    {
         end = _internalClock.restart();
-        stopped= true;
+        stopped = true;
     }
 
-    sf::Time getTime(){
-        return end-begin;
-    }
+    sf::Time getTime() { return end - begin; }
 
     bool stopped = false;
     std::string name;
+
   private:
-    sf::Time begin,end;
+    sf::Time begin, end;
     sf::Clock _internalClock;
 };
 
-//Small Profiler that can be used to track different parts of the code
+// Small Profiler that can be used to track different parts of the code
 class Profiler {
   public:
-    enum ProfilerMode{MILLIS,MICRO};
-    Profiler(){
-        _last50times.resize(50);
+    enum class ProfilerMode {
+        Milliseconds,
+        Microseconds,
     };
-    ProfilerMode Mode = MICRO;
-    void reset(){
+    Profiler();
+    void reset();
+    void onGUI();
 
-        auto temp =_internalClock.restart();
+    TimeSlot& newTimeslot(const char* name);
 
-        Frametime = temp.asSeconds();
-        FrametimeMicro = temp.asMicroseconds();
-        FrametimeMilli = temp.asMilliseconds();
-        _last50times.push_back(Frametime);
-        if(_last50times.size()>50){_last50times.pop_front();}
+    float frametime = 0;
+    float frametimeAverage = 0;
+    ProfilerMode Mode = ProfilerMode::Milliseconds;
 
-        float tmpavg;
-        for(auto& x:_last50times){tmpavg+=x;}
-        FrametimeAverage = tmpavg/0.05;
-
-        for(auto& x:_activeslots){
-
-            if(!x.stopped){
-                x.stop();
-                _lastSlots.emplace_back(std::make_pair("ForgotToStop: "+x.name,0.f));
-                continue;
-            }
-            _lastSlots.emplace_back(std::make_pair(x.name,Mode==MILLIS?x.getTime().asMilliseconds():x.getTime().asMicroseconds()));
-        }
-        _activeslots.clear();
-    };
-    void onGUI(){
-        if (ImGui::Begin("Profiler")) {
-            ImGui::TextColored({0, 0.392, 0.960,1},("Current Frametime:"+std::to_string(FrametimeMilli)+"ms").c_str());
-            std::array<float,50> _temptimes;
-            for(int x =0;x<_temptimes.size();x++){
-                _temptimes[x]=_last50times[x];
-            }
-            ImGui::PlotHistogram("##Times",_temptimes.data(),_last50times.size());
-            ImGui::Text("Avg: %.2f ms,(%.1f fps)",FrametimeAverage,1000.f/FrametimeAverage);
-            ImGui::Separator();
-            ImGui::TextColored({0, 0.6, 0.188,1},"Times in %s", Mode==MILLIS?"Milliseconds":"Microseconds");
-            std::vector<float> temptime;
-            for(auto& ts : _lastSlots){
-                ImGui::Text((ts.first+":"+std::to_string(ts.second)).c_str());
-                temptime.emplace_back(ts.second);
-            }
-            ImGui::PlotHistogram("Times",temptime.data(),temptime.size());
-        }
-        ImGui::End();
-        _lastSlots.clear();
-    };
-    TimeSlot& newTimeslot(std::string name){
-        return _activeslots.emplace_back(name);
-    };
-    int FrametimeMilli;
-    int FrametimeMicro;
-    float Frametime;
-    float FrametimeAverage;
   private:
-    std::deque<float> _last50times;
-    std::vector<TimeSlot> _activeslots;
-    std::vector<std::pair<std::string,float>> _lastSlots;
-    sf::Clock _internalClock;
+    std::deque<float> m_last50times;
+    std::vector<TimeSlot> m_activeslots;
+    std::vector<std::pair<std::string, float>> m_lastSlots;
+    sf::Clock m_internalClock;
 };
