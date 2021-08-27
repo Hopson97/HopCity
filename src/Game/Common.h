4f32d93978cd33dfc2f1cbeb2de035fbdcf54146
Hopson97 <mhopson@hotmail.co.uk>
2021-08-27 22:39:11 +0100

    Fix the grid map not displaying in the correct locations

--- src/Game/Common.h
+++ src/Game/Common.h
@@ -3,7 +3,7 @@
 #include <SFML/System/Vector2.hpp>
 #include <cstdint>
 
-constexpr int CHUNK_SIZE = 64;
+constexpr int CHUNK_SIZE = 128;
 
 constexpr float TILE_HEIGHT = 16.0f;
 constexpr float TILE_WIDTH = 32.0f;
